7b99da69cc8cc382d1d7b7dcea146739f70a6aad
Frédéric Carisey <frederic.carisey@xorivia.com>
2021-06-09 19:47:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,20 +18,29 @@ session_start();
 
 if (DEBUG)
     ini_set('display_errors', E_ALL);
+if (DEBUG_MAIL)
+    \Controller\MailController::$smtp_debug = SMTP::DEBUG_SERVER;
 
 require_once("librairie/autoloader.php");
 
-\Database::$database_type = \Database::TYPE_PGSQL;
-\Database::$port = 5432;
+\Database::$database_type = \Database::TYPE_MYSQL;
+\Database::$port = 3306;
 \Database::$dbname = "test";
-\Database::$user = "administrateur";
-\Database::$password = "6bub94z4";
+\Database::$user = "root";
+\Database::$password = "";
 \Database::$tables = [
-    "user" => "user"
+    'user' => "user"
 ];
 
-if (DEBUG_MAIL)
-    \Controller\MailController::$smtp_debug = SMTP::DEBUG_SERVER;
+\Controller\UserController::INSERT([
+    'username' => "venntus17",
+    'first_name' => "Frédéric",
+    'last_name' => "carisey",
+    'mail' => "venntus17@gmail.com",
+    'tel' => "07 79 55 34 66",
+    'password' => "DEFAULT",
+    'role' => "customer"
+]);
 
 \Controller\ViewController::process();
 
