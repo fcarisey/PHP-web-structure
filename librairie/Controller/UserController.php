80517843725783d125d4aa41dbbc2d8551352b8c
Frédéric Carisey <frederic.carisey@xorivia.com>
2021-07-02 14:09:18 +0200

    rem

--- librairie/Controller/UserController.php
+++
@@ -1,7 +0,0 @@
-<?php
-    namespace Controller;
-    Class UserController extends ControllerController{
-        protected static $table_name = "user";
-        protected static $model_class = \Model\User::class;
-        protected static $database = "db1";
-    }
